5c9515fb0156ea67a03b72b25736c82045f2df60
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2023-03-16 05:14:37 +0500

    add repository to github

---
+++ exercis_3.py
@@ -0,0 +1,10 @@
+# Определить, какие из слов «attribute», «класс», «функция», «type»
+# невозможно записать в байтовом типе с помощью маркировки b''
+# (без encode decode).
+var2 = b'attribute'
+var3 = b'класс'
+var4 = b'функция'
+var5 = b'type'
+ #File
+#"/\Users\иир\AppData\Local\Programs\Python\Python310\python.exe C:\Users\иир\Desktop\урок\seminar_10\exercis_3.py ",line 3 var3 = b'класс'
+#SyntaxError: bytes can only contain ASCII literal characters
